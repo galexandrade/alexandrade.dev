9130b42fdd9e7b2a6331067d27acd57f352cf83e
Alex Andrade <g.alex.andrade@gmail.com>
2022-01-19 09:43:37 -0600

    persisting the profile information

--- app/routes/admin/profile.tsx
+++ app/routes/admin/profile.tsx
@@ -1,12 +1,24 @@
-import type { LinksFunction, MetaFunction, ActionFunction } from 'remix';
+import {
+    LinksFunction,
+    MetaFunction,
+    ActionFunction,
+    useLoaderData,
+    LoaderFunction,
+    redirect,
+} from 'remix';
 import { useActionData, json } from 'remix';
 import stylesUrl from '~/styles/admin/posts.css';
+import stylesProfileUrl from '~/styles/admin/profile.css';
 import { authenticateFirebaseFromSession } from '~/api/auth';
 import { logout } from '~/api/session';
 import AdminLayout from '~/components/AdminLayout';
+import { getAboutContent, saveAbout } from '~/api/profile';
 
 export let links: LinksFunction = () => {
-    return [{ rel: 'stylesheet', href: stylesUrl }];
+    return [
+        { rel: 'stylesheet', href: stylesUrl },
+        { rel: 'stylesheet', href: stylesProfileUrl },
+    ];
 };
 
 // https://remix.run/api/conventions#meta
@@ -15,6 +27,10 @@ export let meta: MetaFunction = () => {
         title: 'About Alex P. Andrade',
         description: 'About Alex P. Andrade',
     };
+};
+
+export const loader: LoaderFunction = async () => {
+    return getAboutContent();
 };
 
 const badRequest = (data: ActionData) => json(data, { status: 400 });
@@ -26,19 +42,45 @@ export const action: ActionFunction = async ({ request }) => {
         return logout(request);
     }
 
-    return badRequest({
-        formError: `Form not submitted correctly.`,
-    });
+    const form = await request.formData();
+    const about = form.get('about');
+
+    if (!about) {
+        return badRequest({
+            formError: `Form not submitted correctly.`,
+        });
+    }
+
+    await saveAbout({ content: about });
+    return redirect('/admin/profile');
 };
 type ActionData = {
     formError?: string;
 };
 
 export default function Profile() {
-    useActionData<ActionData>();
+    const about = useLoaderData();
+    const actionData = useActionData<ActionData>();
     return (
         <AdminLayout>
-            <h1>Profile</h1>
+            <h1>Manage Profile</h1>
+            <form method="post">
+                <div>
+                    <label htmlFor="about-input">About</label>
+                    <textarea
+                        id="about-input"
+                        name="about"
+                        defaultValue={about.raw}
+                        rows={50}
+                    />
+                </div>
+                <div>
+                    <button className="save-button">Save</button>
+                    <a href="/admin/posts" className="cancel-button">
+                        Cancel
+                    </a>
+                </div>
+            </form>
         </AdminLayout>
     );
 }
